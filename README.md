a26a6c91529c3c2ff1cccd33c90ca41b126a71e9
Misa <you@example.com>
2021-09-09 22:51:06 -0500

    configuring prettier, eslint, npm

--- README.md
+++ README.md
@@ -13,20 +13,27 @@
 - ReactDOM.render(micomponent, document.getElementById("theidwheretorender")) it is needed to link the component to render with the div that will render the component
 - When there are multiple children in a list, each one should have a unique "key" prop
 - In react the data flows only in one way. From parents to children and not the other way around. Is not possible for a child to pass info to its parent.
+- You never modify package-lock.json manually. It is always going to be updated by npm
+- Prettier needs a .prettierrc which is the configuration file for prettier
+- ESlint gives you advice about some good practices when coding
 
 ## Tools
-| Name | Where to find it |
-|--|--|
+
+| Name           | Where to find it      |
+| -------------- | --------------------- |
 | Material Icons | VS plugin marketplace |
-| unpkg is a fast, global content delivery network for everything on npm. Use it to quickly and easily load any file from any package using a URL like: unpkg.com/:package@:version/:file | https://unpkg.com |
-|  |  |
-|  |  |
-|  |  |
+
+| unpkg is a fast, global content delivery network for everything on npm.
+Use it to quickly and easily load any file from any package using a URL like: unpkg.com/:package@:version/:file | https://unpkg.com |
+| Prettier https://github.com/prettier/prettier | Lets you format your code and provides good practices |
+| Prettier | As a VScode extension |
+| ESlint | As a VScode extension |
 
 ## Shortcuts
-| Shortcut | Action |
-|--|--|
-| html:5 | Creates a basic html template |
-|  |  |
-|  |  |
-|  |  |
+
+| Shortcut                                     | Action                                          |
+| -------------------------------------------- | ----------------------------------------------- |
+| html:5                                       | Creates a basic html template                   |
+| npm init                                     | Adds the basic structure that is a package.json |
+| npm install -D prettier                      | To install prettier as a DEV dependency         |
+| npm install -D eslint eslint-config-prettier | To install eslint as dev dependency             |
--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+//There are some notations for eslint that lets you ignore some things
+// this ignores the no declaration of the react libraries
+// ->> /*global React ReactDOM*/
+
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div", //this is the html label to create
-        {}, //this is an empty object. here we can define attributes and pass it to the element like {id : "my-brand"}
-        [
-            React.createElement("h1", {}, "Adopt Me!"), //optionally passing additional content to the div. We can pass a String too
-            React.createElement(Pet, {name: "s", animal: "d", breed:"s"}),
-            React.createElement(Pet, {name: "q", animal: "x", breed:"s"}),
-            React.createElement(Pet,  {name: "e", animal: "4e", breed:"w3"})
-        ]
-    );
+  return React.createElement(
+    "div", //this is the html label to create
+    {}, //this is an empty object. here we can define attributes and pass it to the element like {id : "my-brand"}
+    [
+      React.createElement("h1", {}, "Adopt Me!"), //optionally passing additional content to the div. We can pass a String too
+      React.createElement(Pet, {
+        name: "sasdsadadsa",
+        animal: "dadsasdasdasd",
+        breed: "sadasdasd",
+      }),
+      React.createElement(Pet, {
+        name: "q123123",
+        animal: "x123123",
+        breed: "s123123213",
+      }),
+      React.createElement(Pet, {
+        name: "edf23fds",
+        animal: "4e234fwdf",
+        breed: "w3234fwef2",
+      }),
+    ]
+  );
 };
 
 // Below ReactDOM will render the component. In this case root is the div id that we choose.
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
